2836373f276be89b5ccf51ae8e234563501d9578
mateus-earth <mateus@cosmicpig.digital>
2023-02-27 17:15:30 -0300

    Demo is runing again...

--- source/main.js
+++ source/main.js
@@ -16,14 +16,17 @@
 //                                                                            //
 //---------------------------------------------------------------------------~//
 
+
 //----------------------------------------------------------------------------//
 // Constants                                                                  //
 //----------------------------------------------------------------------------//
-const PROJECT_TITLE        = "<b>Title:</b>Game of Life<br>";
-const PROJECT_DATE         = "<b>Date:</b>Jul 17, 2020<br>";
-const PROJECT_VERSION      = "<b>Version:</b> " + GetVersion() + "<br>";
-const PROJECT_INSTRUCTIONS = "<br>Click and drag to add more cells<br>";
-const PROJECT_LINK         = "<a href=\"http://stdmatt.com/demos/game_of_life.html\">More info</a>";
+//------------------------------------------------------------------------------
+__SOURCES = [
+    "/modules/demolib/modules/external/chroma.js",
+    "/modules/demolib/modules/external/gif.js/gif.js",
+
+    "/modules/demolib/source/demolib.js",
+];
 
 const TIME_TO_UPDATE    = 0.10;
 const CELL_SIZE_DEFAULT = 5;
@@ -31,14 +34,13 @@ const CLEAR_COLOR       = "black";
 
 const COLOR_CHANGE_MULTIPLIER = 0.1;
 
-
 //----------------------------------------------------------------------------//
 // Variables                                                                  //
 //----------------------------------------------------------------------------//
 let CurrentTime    = 0;
 let NextUpdateTime = 0;
 
-let CellSize = CELL_SIZE_DEFAULT;
+let CellSize  = CELL_SIZE_DEFAULT;
 let CurrState = null;
 let NextState = null;
 
@@ -53,53 +55,7 @@ let mouse_last_x  = -1;
 let mouse_last_y  = -1;
 let mouse_moved   = false;
 
-
-//----------------------------------------------------------------------------//
-// Helper Functions                                                           //
-//----------------------------------------------------------------------------//
-//------------------------------------------------------------------------------
-function
-ConfigureCanvas()
-{
-    const parent        = document.getElementById("canvas_div");
-    const parent_width  = parent.clientWidth;
-    const parent_height = parent.clientHeight;
-
-    const max_side = Math_Max(parent_width, parent_height);
-    const min_side = Math_Min(parent_width, parent_height);
-
-    const ratio = min_side / max_side;
-
-    // Landscape
-    if(parent_width > parent_height) {
-        Canvas_CreateCanvas(800, 800 * ratio, parent);
-    }
-    // Portrait
-    else {
-        Canvas_CreateCanvas(800 * ratio, 800, parent);
-    }
-
-    Canvas.style.width  = "100%";
-    Canvas.style.height = "100%";
-}
-
-//------------------------------------------------------------------------------
-function
-AddInfo()
-{
-    const info = document.createElement("p");
-    info.innerHTML = String_Cat(
-        PROJECT_TITLE,
-        PROJECT_DATE,
-        PROJECT_VERSION,
-        PROJECT_INSTRUCTIONS,
-        PROJECT_LINK,
-    )
-
-    const parent = document.getElementById("canvas_div");
-    parent.appendChild(info);
-}
-
+let background_color = 0xFF00FF;
 
 //----------------------------------------------------------------------------//
 // Game Implementation                                                        //
@@ -110,17 +66,17 @@ CreateGame(cell_size)
 {
     CellSize = cell_size;
 
-    FieldRows = Math_Int(Canvas_Height / CellSize);
-    FieldCols = Math_Int(Canvas_Width  / CellSize);
+    FieldRows = to_int(get_canvas_height() / CellSize);
+    FieldCols = to_int(get_canvas_width()  / CellSize);
 
     console.log(FieldRows, FieldCols);
 
-    CurrState = Array_Create2D(FieldRows, FieldCols, null);
-    NextState = Array_Create2D(FieldRows, FieldCols, null);
+    CurrState = create_2d_array(FieldRows, FieldCols, null);
+    NextState = create_2d_array(FieldRows, FieldCols, null);
 
     for(let i = 0; i < FieldCols * FieldRows / 2; ++i) {
-        const y = Random_Int(0, FieldRows);
-        const x = Random_Int(0, FieldCols);
+        const y = random_int(0, FieldRows);
+        const x = random_int(0, FieldCols);
 
         CurrState[y][x] = true;
     }
@@ -137,7 +93,7 @@ CountNeighbours(state, y, x)
             continue;
         }
 
-        const ii = Math_Wrap(0, state.length-1, i);
+        const ii = wrap_around(i, 0, state.length-1);
         for(let j = x-1; j <= x+1; ++j) {
             const j_out_of_bounds = (j < 0 || j >= state[ii].length);
             if(j_out_of_bounds && !AllowWrap) {
@@ -148,7 +104,7 @@ CountNeighbours(state, y, x)
                 continue;
             }
 
-            const jj = Math_Wrap(0, state[ii].length-1, j);
+            const jj = wrap_around(j, 0, state[ii].length-1);
             if(state[ii][jj]) {
                 ++count;
             }
@@ -166,7 +122,7 @@ ApplyRules()
     // 2 - Any live cell with two or three live neighbours lives on to the next generation.
     // 3 - Any live cell with more than three live neighbours dies, as if by overpopulation.
     // 4 - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-    NextState = Array_Create2D(FieldRows, FieldCols, null);
+    NextState = create_2d_array(FieldRows, FieldCols, null);
 
     const rows = CurrState.length;
     const cols = CurrState[0].length;
@@ -204,12 +160,11 @@ ApplyRules()
 function
 DrawCurrState()
 {
-    Canvas_SetFillStyle(CLEAR_COLOR);
-    Canvas_FillRect(0, 0, Canvas_Width, Canvas_Height);
+    clear_canvas(CLEAR_COLOR)
 
     //
-    const v = Math_Abs(Math_Sin(Time_Total * COLOR_CHANGE_MULTIPLIER));
-    Canvas_SetFillStyle(chroma.hsl(360 * v, 0.5, 0.5));
+    const v = Math.abs(Math.sin(get_total_time() * COLOR_CHANGE_MULTIPLIER));
+    set_canvas_fill(chroma.hsl(360 * v, 0.5, 0.5));
 
     const rows = CurrState.length;
     const cols = CurrState[0].length;
@@ -221,53 +176,13 @@ DrawCurrState()
                 continue;
             }
 
-            Canvas_FillRect(
+            fill_rect(
                 j * CellSize,
                 i * CellSize,
                 CellSize - 1,
                 CellSize - 1
             );
         }
-    }
-}
-
-
-//----------------------------------------------------------------------------//
-// Setup / Draw                                                               //
-//----------------------------------------------------------------------------//
-//------------------------------------------------------------------------------
-function
-Setup()
-{
-    //
-    ConfigureCanvas ();
-    AddInfo         ();
-
-    //
-    Random_Seed(null);
-    Input_InstallBasicMouseHandler(Canvas);
-
-    // Game.
-    Canvas_Translate(-Canvas_Half_Width, -Canvas_Half_Height);
-    CreateGame(CellSize);
-
-    // Game Loop.
-    Canvas_Start();
-}
-
-
-//------------------------------------------------------------------------------
-function
-Draw(dt)
-{
-    DrawCurrState();
-    if(mouse_is_down) {
-        return;
-    }
-
-    if(Time_Total > NextUpdateTime) {
-        NextUpdateTime = (Time_Total + TIME_TO_UPDATE);
-        ApplyRules();
     }
 }
 
@@ -303,8 +218,8 @@ OnMouseMove()
 
     mouse_moved = true;
 
-    const x = Math_Int(Mouse_X / CellSize);
-    const y = Math_Int(Mouse_Y / CellSize);
+    const x = to_int(Mouse_X / CellSize);
+    const y = to_int(Mouse_Y / CellSize);
 
     if(mouse_last_x == x && mouse_last_y == y) {
         return;
@@ -324,13 +239,82 @@ OnMouseClick()
         return;
     }
 
-    const x = Math_Int(Mouse_X / CellSize);
-    const y = Math_Int(Mouse_Y / CellSize);
+    const x = to_int(Mouse_X / CellSize);
+    const y = to_int(Mouse_Y / CellSize);
     CurrState[y][x] = !CurrState[y][x];
 }
 
 
+
 //----------------------------------------------------------------------------//
-// Entry Point                                                                //
+// Setup / Draw                                                               //
 //----------------------------------------------------------------------------//
-Setup()
+//------------------------------------------------------------------------------
+function setup_standalone_mode()
+{
+    return new Promise((resolve, reject)=>{
+        demolib_load_all_scripts(__SOURCES).then(()=> { // Download all needed scripts.
+            // Create the standalone canvas.
+            const canvas = document.createElement("canvas");
+
+            canvas.width            = window.innerWidth;
+            canvas.height           = window.innerHeight;
+            canvas.style.position   = "fixed";
+            canvas.style.left       = "0px";
+            canvas.style.top        = "0px";
+            canvas.style.zIndex     = "-100";
+
+            document.body.appendChild(canvas);
+
+            // Setup the listener for gif recording.
+            gif_setup_listeners();
+
+            resolve(canvas);
+        });
+    });
+}
+
+//------------------------------------------------------------------------------
+function setup_common(canvas)
+{
+    set_random_seed();
+    set_main_canvas(canvas);
+    install_input_handlers(canvas);
+
+    CreateGame(CellSize);
+
+    start_draw_loop(draw);
+}
+
+
+
+//------------------------------------------------------------------------------
+function demo_main(user_canvas)
+{
+    if(!user_canvas) {
+        setup_standalone_mode().then((canvas)=>{
+            setup_common(canvas);
+        });
+    } else {
+        canvas = user_canvas;
+        setup_common();
+    }
+
+}
+
+//------------------------------------------------------------------------------
+function draw(dt)
+{
+    clear_canvas(background_color);
+    begin_draw();
+        DrawCurrState();
+        if(mouse_is_down) {
+            return;
+        }
+
+        if(get_total_time() > NextUpdateTime) {
+            NextUpdateTime = (get_total_time() + TIME_TO_UPDATE);
+            ApplyRules();
+        }
+    end_draw();
+}
